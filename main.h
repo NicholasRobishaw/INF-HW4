50322da3513937d3e1c4c2e72af9985242914805
Nicholas Robishaw <128092817+NicholasRobishaw@users.noreply.github.com>
2024-07-18 14:48:13 -0700

    First run for problem 1

--- main.h
+++ main.h
@@ -11,23 +11,18 @@ using namespace std;
 
 #ifndef MAIN_H
 
-    class Queries_NW{
-        int neddleman_Wunsch( string oneString, string queryString);
-        void search_NW(bool isQuery, long searchMax);
-        string random_Fragment_Str( int size);
-        long random_Index(bool isQuery);
-        bool read_Qurey(const string& file_Name);
-        void query_Constructor(const string newQuery);
-        bool file_reader(const string& file_Name);
-        void genome_Constructor(const string toAdd, long catIndex);
-        void qurey_Deconstructor(char** queryPtr, int size);
-        void genome_Deconstructor(char* subjectPtr);
+    
+int neddleman_Wunsch( string oneString, string queryString);
+void search_NW(bool isQuery, long searchMax);
+string random_Fragment_Str( int size);
+long random_Index(bool isQuery);
+bool read_Qurey(const string& file_Name);
+void query_Constructor(const string newQuery);
+bool file_reader(const string& file_Name);
+void genome_Constructor(const string toAdd, long catIndex);
+void qurey_Deconstructor(char** queryPtr, int size);
+void genome_Deconstructor(char* subjectPtr);
 
-    };
-
-    class Queries_BL{
-
-    };
-
+    
 
 #endif
